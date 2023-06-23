31f186041491abf2a5c50ce9c574a6fa50730bc6
WANGJUNJIE <32323900+wanng-ide@users.noreply.github.com>
2023-06-23 18:57:51 +0900

    Update scan_copy_pdfs.py

--- scan_copy_pdfs.py
+++ scan_copy_pdfs.py
@@ -3,6 +3,15 @@ import random
 import shutil
 from tqdm import tqdm
 
+def scan_pdf_files(source_folder):
+    pdf_files = []
+    for root, dirs, files in tqdm(os.walk(source_folder), desc="扫描文件夹", ncols=100):
+        for file in files:
+            if file.endswith(".pdf"):
+                file_path = os.path.join(root, file)
+                pdf_files.append(file_path)
+    return pdf_files
+
 def copy_random_pdf_files(source_folder, destination_folder, n):
     # 检查源文件夹是否存在
     if not os.path.exists(source_folder):
@@ -13,26 +22,22 @@ def copy_random_pdf_files(source_folder, destination_folder, n):
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
 
-    # 遍历源文件夹，并筛选出pdf文件，同时显示进度条
-    with tqdm(total=n, desc="扫描文件夹", ncols=100) as pbar1:
-        pdf_files = []
-        for entry in os.scandir(source_folder):
-            if entry.is_file() and entry.name.endswith(".pdf"):
-                pdf_files.append(entry.path)
-            pbar1.update(1)
+    # 扫描源文件夹及其子文件夹中的所有PDF文件，并显示进度条
+    pdf_files = scan_pdf_files(source_folder)
 
     # 检查是否有足够的文件可供选择
     if len(pdf_files) < n:
-        print("源文件夹中的pdf文件数量不足")
+        print("源文件夹及其子文件夹中的pdf文件数量不足")
         return
 
     # 随机选择n个pdf文件，并复制到目标文件夹中，同时显示进度条
-    with tqdm(total=n, desc="复制文件", ncols=100) as pbar2:
+    with tqdm(total=n, desc="复制文件", ncols=100) as pbar:
         selected_files = random.sample(pdf_files, n)
         for file in selected_files:
-            destination_path = os.path.join(destination_folder, os.path.basename(file))
+            destination_path = os.path.join(destination_folder, os.path.relpath(file, source_folder))
+            os.makedirs(os.path.dirname(destination_path), exist_ok=True)
             shutil.copyfile(file, destination_path)
-            pbar2.update(1)
+            pbar.update(1)
 
     print(f"成功复制了 {n} 个随机pdf文件到目标文件夹 '{destination_folder}'")
 
