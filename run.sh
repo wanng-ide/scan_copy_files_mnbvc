75c12bd5776af14389c44d98f79a42a5636c2e3c
WANGJUNJIE <32323900+wanng-ide@users.noreply.github.com>
2023-07-14 20:08:33 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python scan_copy_files.py --source_folder ./source_folder \
                             --destination_folder ./target_folder \
-                            --n 100 --file_types pdf
+                            --n 100 --keywords .pdf .doc .png
